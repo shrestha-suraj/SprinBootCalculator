ce26886b934be53eac152fbc6c76d840a590aaf9
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-05-24 09:41:46 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+
+const App=()=> {
+  return (
+    <div className="App"> 
+      <div className="calculatorFrame">
+        <div className="screen">
+            <h1>Suraj</h1>
+        </div>
+        <div className="buttonPanel">
+          <h1>Shrestha</h1>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
